7c16d18f4e36e01867fda0096106e062d9c45efa
EMMANUEL ISRAEL <kingojem@inclide.com>
2018-12-24 03:59:01 +0100

    Static Understanding, Methods,Constructors,Data Items etc.

--- CSharpOOP/CSharpOOP.cs
+++ CSharpOOP/CSharpOOP.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;  //  THIS dEFINES CONSOLE, GIVES ME THE ABILITY TO CALL METHODS LIKE WRITELINE AND READLINE DIRECTLY, FOR A MORE CLEAN CODEBASE
+using static System.DateTime;
+
 
 namespace CSharpOOP
 {
     class OOP
     {
         static void Main(string[] args)
+        {
+            InitializeStaticClass();
+            
+        }
+        static void InitializeCar()
         {
             Car car = new Car();
             car.brand = "Toyota";
@@ -20,14 +28,88 @@ namespace CSharpOOP
             {
                 car.SpeedUp(5);
                 car.DisplayCurrentspeed();
-                if(car.speed > 65)
+                if (car.speed > 65)
                 {
                     Console.WriteLine("Moving Too Fast! \n Slow Down");
                     break;
-                    
+
                 }
             }
-
+        }
+        static void InitializeMotorCycle()
+        {
+            Motorcycle motorcycle = new Motorcycle();
+            motorcycle.DisplayMotorCycleInfo();
+            motorcycle.PopAWheely();
+        }
+        static void InitializeSavingsAccount()
+        {
+            SavingsAccount savingsAccount = new SavingsAccount(4322.33);
+            Console.WriteLine("Interest ADDition To this is : {0}",SavingsAccount.GetInterestAcquired());
+            //SavingsAccount.SetIntrestAcquired(0.454);
+            SavingsAccount savingsAccount1 = new SavingsAccount(678977.4555);
+            Console.WriteLine("Interest ADDition To this is : {0}", SavingsAccount.GetInterestAcquired());
+        }
+        static void InitializeStaticClass()
+        {
+            StaticUndestanding.name = "Emmanuel";
+            StaticUndestanding.PrintTime();
+            StaticUndestanding.PrintDate();
+            WriteLine(StaticUndestanding.PrintName() +"\n" );
         }
     }
+    #region Understanding The Concept OF STAtic Data,MEthods etc.. ; Making A Bank Savings Model
+    class SavingsAccount
+    {
+        
+        public double amountSavedByUser;
+
+        //every 6 Months, Interest acquired BY Customer 
+        public static double savingsInterestAcquired;
+        // public static double savingsInterestAcquired = 087; DOING THIS IS QUITE OKAY
+
+
+        public SavingsAccount(double balance)
+        {
+            amountSavedByUser = balance;
+            ///savingsInterestAcquired = 0.445; DOING THIS WILL MAKE THE DATA ITEM RESET AT EVERY CALL, WHICH IS NOT THE GOAL TO BE ACHIEVE
+            //
+
+            Console.WriteLine($"Your Balance: {amountSavedByUser}");
+
+        }
+        static SavingsAccount() // A STATIC CONSTRUCTOR IS VERY HELPFUL IF YOU WANT TO DEFINE A STATIC DATATYPE PROPERLY, 
+                                //ESPECIALLY WHEN THE DATA COMES FROM A LINK OR FILE OR DATABASE
+        {
+            //default interest
+            Console.WriteLine("In static ctor!");
+            savingsInterestAcquired = 0.445;
+        }
+        //static fields can be changed and Manipulated by static Methods :- Defining A Get And Set Method
+
+        public static void SetIntrestAcquired(double interest) => savingsInterestAcquired = interest;
+        public static double GetInterestAcquired() => savingsInterestAcquired;
+
+
+    }
+
+    ///<summary>
+    ///Creating A Class Marked As Static is more or less for utility, it cant be called via instances, and all fields and method must be tagged 
+    ///or marked as static
+    ///</summary>
+
+    static class StaticUndestanding
+    {
+        public static string name;
+        
+        
+        public static void PrintTime() =>WriteLine(Now.ToShortTimeString());
+        public static string PrintName() => name;
+        public static void PrintDate() => WriteLine();
+
+
+       
+        
+    }
+    #endregion
 }
--- CSharpOOP/Car.cs
+++ CSharpOOP/Car.cs
@@ -14,4 +14,43 @@ namespace CSharpOOP
         public void DisplayCurrentspeed() => Console.WriteLine("Your Current speed {0}MPH",speed);
         public void SpeedUp(int accelerate) => speed += accelerate;
     }
+    class Motorcycle
+    {
+        public string brand;
+        public int speed;
+        public int driverIntensity;
+
+
+        public Motorcycle() : this("", 0) { Console.WriteLine("THe Default Parameter Was ASSIGNED"); }
+        public Motorcycle(string brand) : this(brand, 0) { Console.WriteLine("You Made A NEW BRand BUT DEfault SPEED"); }
+        public Motorcycle(int DefaultSpeed) : this("", DefaultSpeed) { }
+
+        public Motorcycle(string brand, int DefaultSpeed)
+        {
+            this.brand = brand;
+            speed = DefaultSpeed;
+            driverIntensity += (speed / 4);
+            
+        }
+
+       
+
+        public void PopAWheely()
+        {
+            for (int i = 0; i <= driverIntensity; i++)
+            {
+                Console.WriteLine("Yeeeeeee Haaaaaeewww!");
+            }
+        }
+
+       
+        
+
+       
+
+        public void DisplayMotorCycleInfo()
+        {
+            Console.WriteLine($"The MotorCYcle Brand is {brand}\n Default Speed: {speed} \n Your Intencity {driverIntensity}");
+        }
+    }
 }
